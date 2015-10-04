893db6c4c1c96e4a7fe80cb963d881b46bf8bb92
David Gudeman <davidmgudeman@gmail.com>
2015-10-04 01:19:41 -0700

    fixed the genere, need to do javadocs

--- src/com/davidmgudeman/Genre.java
+++ src/com/davidmgudeman/Genre.java
@@ -1,16 +1,16 @@
 package com.davidmgudeman;
+
 import java.util.ArrayList;
 
 import cs1c.SongEntry;
 
 public class Genre
 {
-   //public enum genre {soul_and_reggae, classic_pop_and_rock, 
-  //    dance_and_electronica, folk, punk, metal, jazz_and_blues}
-  
-   
+   // public enum genre {soul_and_reggae, classic_pop_and_rock,
+   // dance_and_electronica, folk, punk, metal, jazz_and_blues}
+
    public int index = 0;
-  
+
    ArrayList<SongEntry> classical = new ArrayList<>();
    ArrayList<SongEntry> soul = new ArrayList<>();
    ArrayList<SongEntry> rock = new ArrayList<>();
@@ -19,81 +19,76 @@ public class Genre
    ArrayList<SongEntry> punk = new ArrayList<>();
    ArrayList<SongEntry> metal = new ArrayList<>();
    ArrayList<SongEntry> jazz = new ArrayList<>();
-   
-    
+
    public Genre(FoothillTunesStore store)
    {
-      
+
       ArrayList<SongEntry> tunes = store.tunes;
-      
+
       for (SongEntry s : tunes)
-      { 
-         
-        if (s.getGenre().equals("classical"))
+      {
+
+         if (s.getGenre().equals("classical"))
          {
-           
-            classical.add(s);  
-         }
-         else if (s.getGenre() == "pop and rock")
-        {
-           rock.add(s);
-        }
-         else if (s.getGenre() == "dance and electronica")
+
+            classical.add(s);
+         } else if (s.getGenre().equals("classic pop and rock"))
+         {
+            rock.add(s);
+         } else if (s.getGenre().equals("dance and electronica"))
          {
             dance.add(s);
-         }
-         else if (s.getGenre() == "folk")
+         } else if (s.getGenre().equals("folk"))
          {
             folk.add(s);
-         }
-         else if (s.getGenre() == "punk")
+         } else if (s.getGenre().equals("punk"))
          {
             punk.add(s);
-         }
-         else if (s.getGenre() == "metal")
+         } else if (s.getGenre().equals("metal"))
          {
             metal.add(s);
-         } else if (s.getGenre() == "jazz and blues")
+         } else if (s.getGenre().equals("jazz and blues"))
          {
             jazz.add(s);
          }
       }
-      
-   /*      printArrayListTunes(classical);
-         printArrayListTunes(rock);
-         printArrayListTunes(dance);
-         printArrayListTunes(folk);
-         printArrayListTunes(punk);
-         printArrayListTunes(metal);
-         printArrayListTunes(jazz);
-     */
    }
+
    public void printArrayListTunes(ArrayList<SongEntry> list)
    {
       int counter = 1;
       for (SongEntry s : list)
       {
-      
-         System.out.println(s.getTitle() + ", " + s.getArtistName() + ", " +  s.getGenre() + ", " + s.getDuration());
-         System.out.println();
+         System.out.println(s.getTitle() + ", " + s.getArtistName() + ", "
+               + s.getGenre() + ", " + s.getDuration());
          counter++;
       }
       System.out.println("The list has " + counter + " elements.");
    }
-   
+
    public void printByGenre()
    {
       System.out.println("CLASSICAL:_________________________________");
       printArrayListTunes(classical);
       System.out.println();
+      System.out.println("ROCK:_________________________________");
       printArrayListTunes(rock);
       System.out.println();
+      System.out.println("DANCE AND ELECTRONIC___________________");
       printArrayListTunes(dance);
       System.out.println();
+      System.out.println("FOLK:_________________________________");
       printArrayListTunes(folk);
+      System.out.println();
+      System.out.println("PUNK:_________________________________");
       printArrayListTunes(punk);
+      System.out.println();
+      System.out.println("METAL:_________________________________");
       printArrayListTunes(metal);
+      System.out.println();
+      System.out.println("JAZZ:_________________________________");
       printArrayListTunes(jazz);
+      System.out.println();
    }
-  
+
 }
